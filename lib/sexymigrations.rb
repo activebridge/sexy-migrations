9125bea7b851d803c3a89377a4ce8c98bb690db7
AlexShmatko <alex.shmatko@active-bridge.com>
2016-06-01 13:54:49 +0300

    Perform deletion of unwanted migrations

--- lib/sexymigrations.rb
+++ lib/sexymigrations.rb
@@ -6,9 +6,23 @@ import File.join(File.dirname(__FILE__), 'tasks', 'db.rake')
 module SexyMigrations
   class Squash
     def call
+      delete_unnecessary_migrations
+    end
+
+    def delete_unnecessary_migrations
+      other_migrations = select_unnecessary_migrations
+      delete_files(other_migrations)
     end
 
     private
+
+    def select_unnecessary_migrations
+      Dir.glob(File.join(migrations_folder, "**/*")).select {|filename| !filename.match(/_create_/) }
+    end
+
+    def delete_files(other_migrations)
+      File.delete(*other_migrations)
+    end
 
     def schema_location
       File.join(root_path, 'db/schema.rb')
